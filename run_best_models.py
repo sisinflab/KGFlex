6a956a4c934a6ae1d66e48bfdf13bbd159ee2b63
alberto <alberto.mancino94@gmail.com>
2022-09-19 16:09:49 +0200

    init

--- run_best_models.py
+++ run_best_models.py
@@ -2,8 +2,8 @@ from elliot.run import run_experiment
 import os
 
 path = ["config_files/facebook-books_best_model.yml",
-        "config_files/yahoo_best_model.yml",
-        "config_files/ml_best_model.yml"]
+        "config_files/yahoo_books_best_model.yml",
+        "config_files/ml_books_best_model.yml"]
 
 for p in path:
     assert os.path.exists(p)
