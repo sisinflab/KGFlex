2f5f4a39661318af35f1116e5d5971afc3adf4e0
alberto <alberto.mancino94@gmail.com>
2022-10-10 13:22:29 +0200

    fix config

---
+++ run_NCF_ML_best.py
@@ -0,0 +1,15 @@
+from elliot.run import run_experiment
+import os
+
+path = ["config_lgcn_ncf_bestmodels/facebook-books_LGCN.yml",
+        "config_lgcn_ncf_bestmodels/facebook-books_NCF.yml",
+        "config_lgcn_ncf_bestmodels/ml_LGCN.yml",
+        "config_lgcn_ncf_bestmodels/ml_NCF.yml",
+        "config_lgcn_ncf_bestmodels/yahoo_LGCN.yml",
+        "config_lgcn_ncf_bestmodels/yahoo_NCF.yml"]
+
+for p in path:
+    assert os.path.exists(p)
+
+for p in path:
+    run_experiment(p)
