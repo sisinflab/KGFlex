9b06734124ff8f916b0ac25d0171cb847f414905
alberto <alberto.mancino94@gmail.com>
2022-10-10 13:23:25 +0200

    ncf ml best run

--- run_NCF_ML_best.py
+++ run_NCF_ML_best.py
@@ -1,12 +1,7 @@
 from elliot.run import run_experiment
 import os
 
-path = ["config_lgcn_ncf_bestmodels/facebook-books_LGCN.yml",
-        "config_lgcn_ncf_bestmodels/facebook-books_NCF.yml",
-        "config_lgcn_ncf_bestmodels/ml_LGCN.yml",
-        "config_lgcn_ncf_bestmodels/ml_NCF.yml",
-        "config_lgcn_ncf_bestmodels/yahoo_LGCN.yml",
-        "config_lgcn_ncf_bestmodels/yahoo_NCF.yml"]
+path = ["config_lgcn_ncf_bestmodels/ml_NCF.yml"]
 
 for p in path:
     assert os.path.exists(p)
