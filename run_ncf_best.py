64d9059886fd542b8a2341d841fad7dd1df7e8e8
alberto <alberto.mancino94@gmail.com>
2022-10-11 19:15:38 +0200

    ncf yahoo best

--- run_ncf_best.py
+++ run_ncf_best.py
@@ -5,6 +5,8 @@ path = ["config_lgcn_ncf_bestmodels/facebook-books_NCF.yml",
         "config_lgcn_ncf_bestmodels/ml_NCF.yml",
         "config_lgcn_ncf_bestmodels/yahoo_NCF.yml"]
 
+path = ["config_lgcn_ncf_bestmodels/yahoo_NCF.yml"]
+
 for p in path:
     assert os.path.exists(p)
 
