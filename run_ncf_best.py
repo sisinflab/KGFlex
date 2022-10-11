3c32ac9da14c11e1e67f822aee207b6b38c30050
alberto <alberto.mancino94@gmail.com>
2022-10-11 09:38:48 +0200

    update best ncf config

---
+++ run_ncf_best.py
@@ -0,0 +1,12 @@
+from elliot.run import run_experiment
+import os
+
+path = ["config_lgcn_ncf_bestmodels/facebook-books_NCF.yml",
+        "config_lgcn_ncf_bestmodels/ml_NCF.yml",
+        "config_lgcn_ncf_bestmodels/yahoo_NCF.yml"]
+
+for p in path:
+    assert os.path.exists(p)
+
+for p in path:
+    run_experiment(p)
